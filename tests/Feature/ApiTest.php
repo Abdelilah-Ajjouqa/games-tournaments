cf81a951fcc5fc8af544f4c2a672be0928ab31f7
Abdelilah <abdouajjouqa@gmail.com>
2025-03-20 11:47:19 +0000

    add login's test

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -33,4 +33,27 @@ class ApiTest extends TestCase
 
         $response->assertStatus(201);
     }
+
+    /** @test */
+    public function it_can_login(){
+
+        $registerData = [
+            'username'=>'abdelilah',
+            'email'=>'abdouajjouqa@gmail.com',
+            'password'=>'password123',
+            'password_confirmation'=>'password123',
+        ];
+
+        $userData = [
+            'email'=>'abdouajjouqa@gmail.com',
+            'password'=>'password123',
+        ];
+
+        $register = $this->post('/api/register', $registerData);
+
+        $response = $this->post('/api/login', $userData);
+
+        $register->assertStatus(201);
+        $response->assertStatus(200);
+    }
 }
