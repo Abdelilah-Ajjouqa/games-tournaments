fdce4e2681b1c7be2e284d625688de4b3ee1e84c
Abdelilah <abdouajjouqa@gmail.com>
2025-03-20 06:49:12 +0000

    add routes needed for posts

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,18 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Auth routes
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+// public routes
+Route::get('/tournaments', [PostController::class, 'index'])->name('index');
+Route::get('/tournament/{id}', [PostController::class, 'show'])->name('show');
+
+
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/tournaments', [PostController::class, 'index'])->name('index');
     Route::post('/tournament', [PostController::class, 'store'])->name('store');
+    Route::put('tournament/{id}', [PostController::class, 'update'])->name('update');
+    Route::delete('/tounrament/{id}', [PostController::class, 'destroy'])->name('destroy');
 });
