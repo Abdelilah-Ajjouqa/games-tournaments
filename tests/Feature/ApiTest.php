2afacd895be534a303bbbf57fc48dffa97a378d5
Abdelilah <abdouajjouqa@gmail.com>
2025-03-20 11:34:06 +0000

    add register test

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -2,19 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    
+    /** @test */
+    public function home(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_can_register(){
+        
+        $userData = [
+            'username'=>'abdelilah',
+            'email'=>'abdouajjouqa@gmail.com',
+            'password'=>'password123',
+            'password_confirmation'=>'password123',
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response->assertStatus(201);
     }
 }
