efc3589bafb5ec00704707ff93c7b0fce33bb8f7
Abdelilah <abdouajjouqa@gmail.com>
2025-03-23 02:34:09 +0000

    add create a new player and show a list of players

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -2,61 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
+use App\Models\tournament;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index($id)
     {
-        //
+        try{
+            $tournament = tournament::findOrFail($id);
+            $players = $tournament->players;
+            return response()->json($players, 200);
+        } catch(\Exception $e){
+            return response()->json(["message"=>"error", "error"=> $e->getMessage()], 404);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(Request $request, $id)
     {
-        //
+        try{
+            $validate = $request->validate([
+                'user_id' => 'required|exists:users,id',
+            ]);
+
+            $player = Player::create([
+                'user_id' => $validate['user_id'],
+                'tournament_id' => $id,
+            ]);
+
+            if(!$player){
+                return response()->json(["message"=>"player not created", "error"=>$player], 400);
+            }
+
+            return response()->json($player, 201);
+
+        } catch(\Exception $e){
+            return response()->json(["message"=>"error", "error"=> $e->getMessage()], 404);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
